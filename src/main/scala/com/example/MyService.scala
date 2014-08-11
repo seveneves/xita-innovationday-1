02a024ad1bdcc48e35087147be78abd66b62c8e7
upeter <peter.urs@gmail.com>
2014-08-11 13:32:18 +0200

    working session id generation test

--- src/main/scala/com/example/MyService.scala
+++ src/main/scala/com/example/MyService.scala
@@ -6,6 +6,8 @@ import directives.LogEntry
 import spray.http._
 import MediaTypes._
 import akka.event.Logging
+import spray.http.HttpHeaders.Cookie
+import java.util.UUID
 
 // we don't implement our route structure directly in the service actor because
 // we want to be able to test it independently, without having to spin up an actor
@@ -25,18 +27,75 @@ class MyServiceActor extends Actor with MyService {
 trait MyService extends HttpService with StaticResources with Api {
 
   def showPath(req: HttpRequest) = req.method match {
-    case HttpMethods.POST => LogEntry("Method = %s, Path = %s, Data = %s" format(req.method, req.uri, req.entity), Logging.InfoLevel)
-    case _ => LogEntry("Method = %s, Path = %s" format(req.method, req.uri), Logging.InfoLevel)
+    case HttpMethods.POST => LogEntry("Method = %s, Path = %s, Data = %s" format (req.method, req.uri, req.entity), Logging.InfoLevel)
+    case _ => LogEntry("Method = %s, Path = %s" format (req.method, req.uri), Logging.InfoLevel)
   }
 
   val myRoute =
     logRequest(showPath _) {
-      staticResources
+      shoppingCartRoutes ~ staticResources
     }
 }
 
 // TODO implement your REST Api
-trait Api extends HttpService {
+trait Api extends HttpService with SessionCookieDirective {
+  implicit val myRejectionHandler = RejectionHandler {
+    case MissingCookieRejection(cookieName) :: _ =>
+      val uuid = UUID.randomUUID().toString()
+      setCookie(HttpCookie("session-id", content = uuid)) {
+        redirect("/session-id-test", StatusCodes.TemporaryRedirect)
+      }
+  }
+  val shoppingCartRoutes =
+    get {
+      path("session-id-test-old") {
+        val uuid = UUID.randomUUID().toString()
+        println(uuid)
+        sessionCookie("session-id", uuid) { sessionCookie =>
+          complete { s"the session cookie is $sessionCookie" }
+        }
+      }
+      path("session-id-test-ok1") {
+        dynamic {
+          val uuid = UUID.randomUUID().toString()
+          setCookie(HttpCookie("session-id", content = uuid)) {
+            complete { s"the session cookie is " }
+          }
+        }
+      }
+      path("set-session") {
+        dynamic {
+          val uuid = UUID.randomUUID().toString()
+          setCookie(HttpCookie("session-id", content = uuid)) {
+            redirect("/session-id-test", StatusCodes.PermanentRedirect)
+          }
+        }
+      }
+
+      path("session-id-test") {
+        cookie("session-id") { sessionCookie =>
+          complete { s"the session cookie is " + sessionCookie }
+        }
+      }
+    }
+  private def genSessionIdCookie = HttpCookie("session-id", content = UUID.randomUUID().toString());
+}
+
+trait SessionCookieDirective extends HttpService {
+
+  def sessionCookie(name: String, newSessionId: => String = UUID.randomUUID().toString()): Directive1[HttpCookie] =
+    headerValue(findCookie(name)) | {
+      val sessionCookie = HttpCookie(name, content = newSessionId);
+      println("generated" + sessionCookie)
+      setCookie(sessionCookie)
+      headerValue { case _ => Some(sessionCookie) }
+    }
+
+  private def genSessionIdCookie(name: String) = HttpCookie(name, content = UUID.randomUUID().toString());
+  private def findCookie(name: String): HttpHeader ⇒ Option[HttpCookie] = {
+    case Cookie(cookies) => cookies.find(_.name == name)
+    case _ => None
+  }
 }
 
 // Trait for serving static resources
@@ -48,11 +107,11 @@ trait StaticResources extends HttpService {
       path("") {
         redirect("/index.html", StatusCodes.PermanentRedirect)
       } ~
-      path("favicon.ico") {
-        complete(StatusCodes.NotFound)
-      } ~
-      path(Rest) { path =>
-        getFromResource("root/%s" format path)
-      }
+        path("favicon.ico") {
+          complete(StatusCodes.NotFound)
+        } ~
+        path(Rest) { path =>
+          getFromResource("root/%s" format path)
+        }
     }
 }
--- src/test/scala/com/example/MyServiceSpec.scala
+++ src/test/scala/com/example/MyServiceSpec.scala
@@ -6,6 +6,7 @@ import spray.http._
 import StatusCodes._
 import org.junit.runner.RunWith
 import org.specs2.runner.JUnitRunner
+import spray.http.HttpHeaders.Cookie
 
 @RunWith(classOf[JUnitRunner])
 class MyServiceSpec extends Specification with Specs2RouteTest with MyService {
@@ -31,6 +32,12 @@ class MyServiceSpec extends Specification with Specs2RouteTest with MyService {
       }
     }
       
+      "GET requests to service must be handled" in {
+      Get("/sesssion-id-test") ~> Cookie(HttpCookie("session-id", "bla")) ~> myRoute ~> check {
+        responseAs[String] startsWith "the session cookie is"
+      }
+    }
+     
     "return a MethodNotAllowed error for PUT requests to the root path" in {
       Put() ~> sealRoute(myRoute) ~> check {
         status === MethodNotAllowed
