12df9126013771a5a3d1f98b771c134deaf1cb0a
upeter <peter.urs@gmail.com>
2014-08-18 17:01:48 +0200

    Fixed loading of products to also work in executable jar

--- application/src/main/scala/com/example/Repositories.scala
+++ application/src/main/scala/com/example/Repositories.scala
@@ -2,53 +2,81 @@ package com.example
 import java.io.File
 import spray.json.JsonParser
 import scala.io.Source
-object ProductRepo {
+import java.net.URI
 
-  def apply(): ProductRepo = {
-    val prods = new File(getClass.getResource("/root/phones/").toURI())
-      .listFiles()
-      .filterNot(_.getName() == "phones.json")
-      .map { file =>
-        val res = Source.fromFile(file).mkString
-        val jsonAst = JsonParser(res)
-        jsonAst.convertTo[Device]
-      }
-    new ProductRepoImpl(prods)
-  }
+/**
+ * Product repository trait
+ */
+trait ProductRepo {
+  val products: Seq[Device]
+  lazy val productMap: Map[String, Device] = products.map(p => p.id -> p).toMap
 }
 
 class ProductRepoImpl(val products: Seq[Device]) extends ProductRepo
 
-trait ProductRepo {
-  val products: Seq[Device]
+/**
+ * Loads products stored in static resources
+ * /webapp/root/phones
+ * /root/\* will be accessible from the classpath
+ */
+object ProductRepo {
 
-  lazy val productMap: Map[String, Device] = products.map(p => p.id -> p).toMap
+  def apply(): ProductRepo = {
+    val products = productFilePaths.map { path =>
+      val productStr = Source.fromInputStream(getClass.getResourceAsStream(path)).mkString
+      val jsonAst = JsonParser(productStr)
+      jsonAst.convertTo[Device]
+    }
+    new ProductRepoImpl(products)
+  }
+
+  /**
+   * Creates a list of paths pointing to each product.json
+   * E.g.: /root/phones/dell-streak-7.json, /root/phones/dell-venue.json etc.
+   *
+   * Necessary to load products this way in order to make it work
+   * in test reading from classpath and reading inside jar
+   */
+  private def productFilePaths: Seq[String] = {
+    val productDirRoot = "/root/phones"
+    import org.json4s._
+    import org.json4s.JsonDSL._
+    import org.json4s.native.JsonMethods._
+    val productsStr = Source.fromInputStream(ProductRepo.getClass.getResourceAsStream(s"$productDirRoot/phones.json")).mkString
+    val jsonAst = parse(productsStr)
+    val productsJStr = (jsonAst \\ "id" \\ classOf[JString])
+    productsJStr.map(p => s"$productDirRoot/$p.json")
+  }
+
 }
 
 
-object SessionRepo extends SessionRepo
+
+/**
+ * Session repository trait
+ */
 trait SessionRepo {
   import collection.mutable._
-  
+
   val sessionState = Map[String, Seq[ShoppingCartItem]]()
-  
-  def removeFromCart(sessionId: String, item: Device):Seq[ShoppingCartItem]  = {
+
+  def removeFromCart(sessionId: String, item: Device): Seq[ShoppingCartItem] = {
     val updatedItems = sessionState.get(sessionId)
       .map(_.filterNot(_.item.id == item.id))
       .getOrElse(Seq())
     sessionState += (sessionId -> updatedItems)
     updatedItems
   }
-  
-  def getCartItems(sessionId:String) = sessionState.get(sessionId).getOrElse(Seq())
-  
-  def checkoutCart(sessionId:String):Seq[ShoppingCartItem]= {
+
+  def getCartItems(sessionId: String) = sessionState.get(sessionId).getOrElse(Seq())
+
+  def checkoutCart(sessionId: String): Seq[ShoppingCartItem] = {
     val items = getCartItems(sessionId)
     sessionState += (sessionId -> Seq())
     items
   }
 
-  def upsertCart(sessionId: String, item: Device):Seq[ShoppingCartItem] = {
+  def upsertCart(sessionId: String, item: Device): Seq[ShoppingCartItem] = {
     val updatedItems = sessionState.get(sessionId) match {
       case Some(items) => {
         val updatedItem = items.find(_.item.id == item.id)
@@ -63,3 +91,5 @@ trait SessionRepo {
 
   }
 }
+object SessionRepo extends SessionRepo
+
